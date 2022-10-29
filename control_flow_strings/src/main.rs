fe4c0b5b3825fab5d2f8f1d18a1a751556d75504
Petr Jenin <petr.enin@gmail.com>
2022-10-29 15:43:12 +0200

    control flow

---
+++ control_flow_strings/src/main.rs
@@ -0,0 +1,62 @@
+// Silence some warnings so they don't distract from the exercise.
+#![allow(dead_code, unused_mut, unused_variables)]
+
+fn main() {
+    // This collects any command-line arguments into a vector of Strings.
+    // For example:
+    //
+    //     cargo run apple banana
+    //
+    // ...produces the equivalent of
+    //
+    //     vec!["apple".to_string(), "banana".to_string()]
+    let args: Vec<String> = std::env::args().skip(1).collect();
+
+    // This consumes the `args` vector to iterate through each String
+    for arg in args {
+        if arg == "sum" {
+            sum();
+        } else if arg == "double" {
+            double();
+        } else {
+            count(arg);
+        }
+    }
+}
+
+fn sum() {
+    let mut sum = 0;
+    for i in 7..=23 {
+        sum += i;
+    }
+
+    println!("The sum is {}", sum);
+}
+
+fn double() {
+    let mut count = 0;
+    let mut x = 1;
+
+    while x < 500 {
+        x *= 2;
+        count += 1;
+    }
+
+    println!("You can double x {} times until x is larger than 500", count);
+}
+
+fn count(arg: String) {
+    // Challenge: Use an unconditional loop (`loop`) to print `arg` 8 times, and then break.
+    // You will need to count your loops, somehow.  Run it with `cargo run bananas`
+
+    let mut count:i32 = 0;
+    loop {
+        count += 1;
+        print!("{} ", arg);
+        if count >= 8 {
+            break;
+        }
+    }
+
+    println!(); // This will output just a newline at the end for cleanliness.
+}
