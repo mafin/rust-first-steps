38ebab359aacc0beae80ea891b1cd34e525d63a2
Petr Jenin <petr.enin@gmail.com>
2022-10-29 15:19:07 +0200

    simple types

---
+++ simple_types/src/main.rs
@@ -0,0 +1,20 @@
+// Silence some warnings so they don't distract from the exercise.
+#![allow(dead_code, unused_variables)]
+
+use simple_types::{print_difference, print_array, ding, on_off, print_distance};
+
+fn main() {
+    let coords: (f32, f32) = (6.3, 15.0);
+    print_difference(coords.1,coords.0);
+
+    let coords_arr :[f32; 2] = [coords.0, coords.1];
+    print_array(coords_arr);
+
+    let series = [1, 1, 2, 3, 5, 8, 13];
+    ding(series[6]);
+
+    let mess = ([3, 2], 3.14, [(false, -3), (true, -100)], 5, "candy");
+    on_off(mess.2[1].0);
+
+    print_distance(coords);
+}
